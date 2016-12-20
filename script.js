f0e022d329fb77065a94680a576b15d8ec37e8df
Drew Silcock <as1423@my.bristol.ac.uk>
2016-12-20 19:11:41 +0000

    Throw errors instead of just logging them.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function getQueryParameterByName(parameterName, url) {
 
   if (!results[2]) {
     // Key found, but value empty.
-    return '';
+    return "";
   }
 
   return decodeURIComponent(results[2].replace(/\+/g, " "));
@@ -23,7 +23,7 @@ function getQueryParameterByName(parameterName, url) {
 
 function makeGetRequest(url, success, error) {
   var request = new XMLHttpRequest();
-  request.open('GET', url, true);
+  request.open("GET", url, true);
 
   request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
@@ -43,37 +43,34 @@ function makeGetRequest(url, success, error) {
 
 // This is being run async anyway, so let's just do it here.
 
-var videoId = getQueryParameterByName('v');
+var videoId = getQueryParameterByName("v");
 
-var iframeElementId = 'DisplayVideo';
-var errorElementId = 'ErrorDisplay';
+var iframeElementId = "DisplayVideo";
+var errorElementId = "ErrorDisplay";
 
 var iframeElement = document.getElementById(iframeElementId);
 var errorElement = document.getElementById(errorElementId);
 
-var googleApiKey = 'AIzaSyBxG_Tf2uvKip5sJLaFUPEjwAx5nd-iJ88'; // Restricted to this domain.
-var videoApiUrl = 'https://www.googleapis.com/youtube/v3/videos?part=id' +
-                  '&id=' + videoId +
-                  '&key=' + googleApiKey;
+var googleApiKey = "AIzaSyBxG_Tf2uvKip5sJLaFUPEjwAx5nd-iJ88"; // Restricted to this domain.
+var videoApiUrl = "https://www.googleapis.com/youtube/v3/videos?part=id" +
+                  "&id=" + videoId +
+                  "&key=" + googleApiKey;
 
 if (!videoId) {
-  console.error("Video ID not found in URL.");
-
-  iframeElement.style.display = 'none';
   alert("You've made a terrible mistake. The `v` parameter for the YouTube " +
                            "video ID wasn't found in the URL.");
+  throw new Error("Video ID not found in URL.");
 }
 
 makeGetRequest(videoApiUrl, function successHandler(data) {
   if (data.items.length === 0) {
-    console.error("Video ID not valid.");
-
     alert("You've made a terrible mistake. The video ID doesn't point to a valid video.");
+    throw new Error("Video ID not valid.");
+  } else {
+    console.log("Video ID is valid.");
   }
-
-  console.log("Video ID is valid.");
 }, function errorHandler() {
-  console.error("There was a problem contacting the Google YouTube API.");
+  throw new Error("There was a problem contacting the Google YouTube API.");
 });
 
 var player;
